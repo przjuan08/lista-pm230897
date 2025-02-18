6aeeaf9d24d2a51f9bf76df1b3ace4890eec3d07
Juan José <jp2004321@gmail.com>
2025-02-18 16:50:22 -0600

    Ejercicio de ejemplo - guia 3

---
+++ src/components/Todo.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+import styles from "../app/page.module.css"
+
+const Todo = ({todo, index, deleteTodo}) => {
+ return (
+    <>
+    <div className={styles.list}> 
+        <h3 className = 'Titulo3'>{todo}</h3> 
+        <button className={styles.btn_delete} onClick={()=>deleteTodo(index)}>X</button> 
+    </div>
+    </>
+    )
+}
+export default Todo
